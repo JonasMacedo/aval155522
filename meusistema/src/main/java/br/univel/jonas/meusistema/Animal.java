c322876807e73125adcc9a524201db185bf641c0
Jonas <Jonas@PCfacul>
2017-06-12 22:09:36 -0300

    Criação do projeto

---
+++ meusistema/src/main/java/br/univel/jonas/meusistema/Animal.java
@@ -0,0 +1,27 @@
+package br.univel.jonas.meusistema;
+
+public class Animal {
+	private String raca;
+	private int idade;
+	private long id;
+	
+	public String getRaca() {
+		return raca;
+	}
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+}
